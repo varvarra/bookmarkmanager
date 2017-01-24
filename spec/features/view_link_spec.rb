ca25a1dc7c18449796e1b84444098e6307b379db
bvjones <benvaughanjones@gmail.com>
2017-01-24 17:42:35 +0000

    Completed exercise 10

--- spec/features/view_link_spec.rb
+++ spec/features/view_link_spec.rb
@@ -1,20 +1,10 @@
-
-
 feature 'Viewing links' do
-
-  # it 'Displaying a list of links' do
-  #   allow(link).to receive(:view) { :value }
-  #   expect(link.view).to eq (:value)
-  # end
 
   scenario 'shows links' do
     Link.create(url: 'http://www.google.com', title: 'Google')
 
-    visit '/link'
+    visit '/links'
     expect(page.status_code).to eq 200
-
-    within 'link' do
-      expect(page).to have_content('Google')
-    end
- end
+    expect(page).to have_content('Google')
+   end
 end
