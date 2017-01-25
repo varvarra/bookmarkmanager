18ba45eac3efa1a886fd44a83832c9fe2c33d108
Jack <92jackhenderson@gmail.com>
2017-01-25 14:44:43 +0000

    updates

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -4,4 +4,15 @@ feature 'Creating links' do
     visit '/links/new'
     expect(page).to have_button("Save Link")
   end
+
+  scenario 'to create a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.twitter.com/'
+    fill_in 'title', with: "Here's my favourite website!"
+    click_button("Save Link")
+    expect(current_path).to eq '/links'
+    # do we need a within here?
+    expect(page).to have_content("Here's my favourite website!")
+  end
+
 end
